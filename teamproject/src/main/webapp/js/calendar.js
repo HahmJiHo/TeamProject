f5a907e11839c69f5182c71280b6d5acb9fc975e
BitCamp <BitCamp@BitCamp-PC>
2016-09-25 18:40:05 +0900

    .

--- teamproject/src/main/webapp/js/calendar.js
+++ teamproject/src/main/webapp/js/calendar.js
@@ -1,28 +1,39 @@
 "use strict";
-$(document).ready(function() {
-	var CalLoading = true;    
-	$('#calendar').fullCalendar({
-		customButtons: {
-	        myCustomButton: {
-	            text: 'save',
-	            click: function() {					
+$(document).ready(function() {    
+	$('#calendar').fullCalendar({		
+		customButtons: {			
+			myCustomButton: {
+				text: 'save',
+				click: function() {	
+					console.log("11");
+					
 					var event = { 
-							title: $('#eventTitle').val(), 
-							start: $('#eventDateStart').val(),
-							end: $('#eventDateEnd').val(),
-					 };
-									
-					if (event.start != null && event.end != null) {
+							title: $('#addeventTitle').val(), 
+							start: $('#addDateStart').val(),
+							end: $('#addDateEnd').val(),
+					};								
+
+					if ($('#addeventTitle').val().length != 0 && $('#addDateStart').val().length != 0 && $('#addDateEnd').val().length != 0) {
 						console.log(event)
-						$('#eventDateStart').val('');
-						$('#eventDateEnd').val('');	
 						$('#calendar').fullCalendar('renderEvent', event, true);									
 						$('#calendar').fullCalendar('addEventSource', event);
 						$('#calendar').fullCalendar('refetchEvents');					
-					}   	
-	           }
-	        }
-	    },
+						swal(
+								'Good job!',
+								'You clicked the button!',
+								'success'							
+						)
+						$('#calendarAddModal').modal('hide');
+					} else {
+						swal(
+								'Good job!',
+								'You clicked the button!',
+								'error'							
+						)						
+					} 	    			
+				}
+			}
+		},
 		header: {
 			left: 'prev,next today myCustomButton',
 			center: 'title',
@@ -30,23 +41,24 @@ $(document).ready(function() {
 		},
 		lang : "ko",
 		navLinks: true, // can click day/week names to navigate views
-		selectable: true,
+		selectable: false,
 		selectHelper: true,
 		disableDragging : true,
-		select: function(start, end) {
+		/*select: function(start, end) {
 			var title = swal({
-				  title: 'Input something',
+				  title: '이벤트를 입력해 주세요',
 				  input: 'text',
 				  showCancelButton: true,
 				  inputValidator: function(value) {
-				    return new Promise(function(resolve, reject) {
+				    return new Promise(function(resolve, reject, title) {
 				      if (value) {
 				        resolve();
+
 				      } else {
 				        reject('You need to write something!');
 				      }
 				    });
-				  }
+				  }			
 				}).then(function(result) {
 				  swal({
 				    type: 'success',
@@ -68,9 +80,18 @@ $(document).ready(function() {
 				$('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
 			}      
 			$('#calendar').fullCalendar('unselect');
-		},
+		},*/
+		dayClick: function(date, jsEvent, view) {
 
+			$('#calendarAddModal').modal();
+
+		},	
 		eventRender: function(event, element, view) {
+			//$('.fc-myCustomButton-button').remove();
+			/*$('.fc-myCustomButton-button').css('display','none');
+			if ($('.fc-myCustomButton-button').length <= 1) {				
+				$(".modal-footer").append('<button type="button" class="fc-myCustomButton-button fc-button fc-state-default fc-corner-left fc-corner-right" style="display:block">save</button>')
+			}*/			
 			var ntoday = new Date().getTime();
 			var eventEnd = moment(event.end).valueOf();
 			var eventStart = moment(event.start).valueOf();
@@ -81,37 +102,25 @@ $(document).ready(function() {
 			if (eventEnd < ntoday) {
 				event.editable = false;
 			}
-			element.find(".closeon").click(function() {         
-				/*
-				var r = confirm("Delete " + event.title);
-				if (r == true) {
-					$('#calendar').fullCalendar('removeEvents', event._id);
-				}              
-				*/
-								
+			element.find(".closeon").click(function() {         								
 				swal({
-					  title: 'Are you sure?',
-					  text: "일정을 삭제 하시겠습니까?",
-					  type: 'warning',
-					  showCancelButton: true,
-					  confirmButtonColor: '#3085d6',
-					  cancelButtonColor: '#d33',
-					  confirmButtonText: 'Yes, delete it!'
-					}).then(function() {
-					  swal(
-					    'Deleted!',
-					    'Your file has been deleted.',
-					    'success'
-					  );
-					  $('#calendar').fullCalendar('removeEvents', event._id);
-					})
-				
-				
+					title: 'Are you sure?',
+					text: "일정을 삭제 하시겠습니까?",
+					type: 'warning',
+					showCancelButton: true,
+					confirmButtonColor: '#3085d6',
+					cancelButtonColor: '#d33',
+					confirmButtonText: 'Yes, delete it!'
+				}).then(function() {
+					swal(
+							'Deleted!',
+							'Your file has been deleted.',
+							'success'
+					);
+					$('#calendar').fullCalendar('removeEvents', event._id);
+				})								
 				return false;
 			});
-			
-			
-
 
 		},
 		eventClick: function(event, start, end) {
@@ -120,20 +129,16 @@ $(document).ready(function() {
 			end = moment(event.end).format('YYYY-MM-DD HH:mm');
 
 			//alert("Event title: " + event.title + " Start Date: " + start + " End Date: " + end );
-			
+
 			$('#modalTitle').html(event.title);
 			$('.modal-start-date').html(start);
 			$('.modal-end-date').html(end);
-            $('#modalBody').html(event.description);
-            $('#eventUrl').attr('href',event.url);
-            $('#calendarModal').modal();
-			
+			$('#modalBody').html(event.description);
+			$('#eventUrl').attr('href',event.url);
+			$('#calendarModal').modal();
 
-		},
-		viewRender:function (view,element) { 	    
-			
-			
-		},	 		
+
+		},	
 		editable: true,
 		eventLimit: true, // allow "more" link when too many events      
 		events: [
@@ -148,26 +153,16 @@ $(document).ready(function() {
 		         {
 
 		        	 title: "Meeting",
-		        	 start: "2016-09-15T10:30:00-05:00",
-		        	 end: "2016-09-18T12:30:00-05:00"
+		        	 start: "2016-09-26T10:30:00-05:00",
+		        	 end: "2016-09-29T12:30:00-05:00"
 		         }
 		         ]
 	});
 	$(function(){
-		$('#eventDateStart').datetimepicker({format:"YYYY-MM-DD HH:mm"}).data('DateTimePicker').date(new Date());
-		$('#eventDateEnd').datetimepicker({format:"YYYY-MM-DD HH:mm"}).data('DateTimePicker').date(new Date());
-		$('#eventDateStart').val('');
-		$('#eventDateEnd').val('');				
-		
+		$('#addDateStart').datetimepicker({format:"YYYY-MM-DD HH:mm"}).data('DateTimePicker').date(new Date());
+		$('#addDateEnd').datetimepicker({format:"YYYY-MM-DD HH:mm"}).data('DateTimePicker').date(new Date());
+		$('#addDateStart').val('');
+		$('#addDateEnd').val('');
+		$('#addeventTitle').val('');
 	});
 });   
-
-/* 모바일..
-   var filter = "win16|win32|win64|mac";
-
-   if (navigator.platform) {
-     if(0 > filter.indexOf(navigator.platform.toLowerCase())) {
-       $('.mobile-add-caledar').css('display','block')
-     }
-   }
- */
